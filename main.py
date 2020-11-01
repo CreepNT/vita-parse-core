36106189bb3c39cb6747fc234aa939fbf272983f
Crêpe N'Tea <creepnt69@gmail.com>
2020-11-01 13:29:22 +0100

    Add option to dump data on all threads
    Usefull for RE w/ REX/DEX coredumps

--- main.py
+++ main.py
@@ -66,6 +66,10 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("-s", "--stack-size-to-print", dest="stacksize",
                         type=int, help="Number of addresses of the stack to print", metavar="SIZE", default=24)
+    parser.add_argument("-m", "--dump-module-info", dest="dumpmodule",
+                        action="store_true", help="Enable dumping of the modules' info.", default=False)
+    parser.add_argument("-A", "--dump-all-threads", dest="dumpall",
+                        action="store_true", help="Enables dumping of all threads' infos, even the non-crashed ones.", default=False)
     parser.add_argument("corefile")
     parser.add_argument("elffile")
     args = parser.parse_args()
@@ -73,11 +77,12 @@ def main():
 
     elf = ElfParser(args.elffile)
     core = CoreParser(args.corefile)
-    # iprint("=== MODULES ===")
-    # with indent():
-    #     for module in core.modules:
-    #         print_module_info(module)
-    # iprint()
+    if (args.dumpmodule):
+        iprint("=== MODULES ===")
+        with indent():
+            for module in core.modules:
+                print_module_info(module)
+        iprint()
     iprint("=== THREADS ===")
     crashed = []
     with indent():
@@ -87,7 +92,7 @@ def main():
             print_thread_info(thread, elf)
     iprint()
     for thread in crashed:
-        iprint('=== THREAD "{}" <0x{:x}> CRASHED ({}) ==='.format(thread.name, thread.uid, str_stop_reason[thread.stop_reason]))
+        iprint('=== THREAD "{}" <0x{:x}> CRASHED ({})  ==='.format(thread.name, thread.uid, str_stop_reason[thread.stop_reason]))
 
         pc = core.get_address_notation('PC', thread.pc)
         pc.print_disas_if_available(elf)
@@ -119,6 +124,47 @@ def main():
                         prefix = "SP =>"
                     data_notation = core.get_address_notation("{} 0x{:x}".format(prefix, addr), data)
                     iprint(data_notation.to_string(elf))
+
+    if(args.dumpall):
+        iprint()
+        non_crashed = []
+        with indent():
+            for thread in core.threads:
+                if not (thread in crashed):
+                    non_crashed.append(thread)
+        for thread in non_crashed:
+            iprint('=== THREAD "{}" <0x{:x}> INFO ==='.format(thread.name, thread.uid))
+
+            pc = core.get_address_notation('PC', thread.pc)
+            pc.print_disas_if_available(elf)
+            lr = core.get_address_notation('LR', thread.regs.gpr[14])
+            lr.print_disas_if_available(elf)
+
+            iprint("REGISTERS:")
+            with indent():
+                for x in range(14):
+                    reg = reg_names.get(x, "R{}".format(x))
+                    iprint("{}: 0x{:x}".format(reg, thread.regs.gpr[x]))
+
+                iprint(pc)
+                iprint(lr)
+                
+                
+            iprint()
+
+            iprint("STACK CONTENTS AROUND SP:")
+            with indent():
+                sp = thread.regs.gpr[13]
+                for x in range(-16, stackSize):
+                    addr = 4 * x + sp
+                    data = core.read_vaddr(addr, 4)
+                    if data:
+                        data = u32(data, 0)
+                        prefix = "     "
+                        if addr == sp:
+                            prefix = "SP =>"
+                        data_notation = core.get_address_notation("{} 0x{:x}".format(prefix, addr), data)
+                        iprint(data_notation.to_string(elf))
 
 if __name__ == "__main__":
     main()
